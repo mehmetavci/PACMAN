46754ba867cba37a15c17a44bf79c420cf12e913
Mehmet Avcı <Mehmet Avc>
2016-11-19 21:35:14 +0300

    PACMANFinish

---
+++ Project-PacmanGame/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Project_PacmanGame
+{
+    partial class FormPacman
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPacman));
+            this.timerPacmanDirection = new System.Windows.Forms.Timer(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pbPacman = new System.Windows.Forms.PictureBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.imageListGhost = new System.Windows.Forms.ImageList(this.components);
+            this.timerGhostDirection = new System.Windows.Forms.Timer(this.components);
+            this.pbHak3 = new System.Windows.Forms.PictureBox();
+            this.pbHak2 = new System.Windows.Forms.PictureBox();
+            this.pbHak1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPacman)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHak3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHak2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHak1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timerPacmanDirection
+            // 
+            this.timerPacmanDirection.Tick += new System.EventHandler(this.timerPacmanDirection_Tick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "pacmanRight.png");
+            this.imageList1.Images.SetKeyName(1, "pacmanLeft.png");
+            this.imageList1.Images.SetKeyName(2, "pacmanDown.png");
+            this.imageList1.Images.SetKeyName(3, "pacmanUp.png");
+            // 
+            // pbPacman
+            // 
+            this.pbPacman.BackColor = System.Drawing.Color.Transparent;
+            this.pbPacman.BackgroundImage = global::Project_PacmanGame.Properties.Resources.Pacman;
+            this.pbPacman.Location = new System.Drawing.Point(37, 37);
+            this.pbPacman.Name = "pbPacman";
+            this.pbPacman.Size = new System.Drawing.Size(50, 50);
+            this.pbPacman.TabIndex = 0;
+            this.pbPacman.TabStop = false;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblScore.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblScore.Location = new System.Drawing.Point(1148, 203);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(0, 20);
+            this.lblScore.TabIndex = 2;
+            // 
+            // imageListGhost
+            // 
+            this.imageListGhost.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListGhost.ImageStream")));
+            this.imageListGhost.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListGhost.Images.SetKeyName(0, "Pacman-orange-c1lyde.sh.png");
+            this.imageListGhost.Images.SetKeyName(1, "Pacman-orange-clyde.sh.png");
+            this.imageListGhost.Images.SetKeyName(2, "GreenGhost.png");
+            // 
+            // timerGhostDirection
+            // 
+            this.timerGhostDirection.Tick += new System.EventHandler(this.timerGhostDirection_Tick);
+            // 
+            // pbHak3
+            // 
+            this.pbHak3.Image = global::Project_PacmanGame.Properties.Resources.Pacman;
+            this.pbHak3.Location = new System.Drawing.Point(1131, 12);
+            this.pbHak3.Name = "pbHak3";
+            this.pbHak3.Size = new System.Drawing.Size(50, 50);
+            this.pbHak3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbHak3.TabIndex = 7;
+            this.pbHak3.TabStop = false;
+            // 
+            // pbHak2
+            // 
+            this.pbHak2.Image = global::Project_PacmanGame.Properties.Resources.Pacman;
+            this.pbHak2.Location = new System.Drawing.Point(1131, 68);
+            this.pbHak2.Name = "pbHak2";
+            this.pbHak2.Size = new System.Drawing.Size(50, 50);
+            this.pbHak2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbHak2.TabIndex = 8;
+            this.pbHak2.TabStop = false;
+            // 
+            // pbHak1
+            // 
+            this.pbHak1.Image = global::Project_PacmanGame.Properties.Resources.Pacman;
+            this.pbHak1.Location = new System.Drawing.Point(1131, 124);
+            this.pbHak1.Name = "pbHak1";
+            this.pbHak1.Size = new System.Drawing.Size(50, 50);
+            this.pbHak1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbHak1.TabIndex = 9;
+            this.pbHak1.TabStop = false;
+            // 
+            // FormPacman
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1203, 560);
+            this.Controls.Add(this.pbHak1);
+            this.Controls.Add(this.pbHak2);
+            this.Controls.Add(this.pbHak3);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pbPacman);
+            this.Name = "FormPacman";
+            this.Text = "Pacman";
+            this.Load += new System.EventHandler(this.FormPacman_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormPacman_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPacman)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHak3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHak2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHak1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timerPacmanDirection;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pbPacman;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.ImageList imageListGhost;
+        private System.Windows.Forms.Timer timerGhostDirection;
+        private System.Windows.Forms.PictureBox pbHak3;
+        private System.Windows.Forms.PictureBox pbHak2;
+        private System.Windows.Forms.PictureBox pbHak1;
+    }
+}
+
---
+++ Project-PacmanGame/Form1.cs
@@ -0,0 +1,439 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_PacmanGame
+{
+    public partial class FormPacman : Form
+    {
+        public FormPacman()
+        {
+            InitializeComponent();
+        }
+
+        List<PictureBox> ForageList = new List<PictureBox>();
+        string[] directionsArray = { "Right", "Left", "Up", "Down" };
+        Random rnd = new Random();
+        string direction = "Right";
+        int score, hak = 3;
+        PictureBox ghost1, ghost2,ghost3;
+
+        public void Win()
+        {
+            timerPacmanDirection.Stop();
+            timerGhostDirection.Stop();
+            DialogResult result = MessageBox.Show("Tebrikler !!!!" + "\n Tekrar Oynamak İster Misiniz?", "Pacman", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+            if (result == DialogResult.OK)
+            {
+                ResetEverything();
+
+            }
+            else if (result == DialogResult.Cancel)
+            {
+                ApplicationExit();
+            }
+
+            pbPacman.Location = new Point(37, 37);
+            pbPacman.BackgroundImage = imageList1.Images[0];
+            pbPacman.BackColor = Color.Transparent;
+            direction = "Right";
+        }
+
+        public void GoToReturn()
+        {
+            hak--;
+            if (hak == 2)
+            {
+                pbHak1.Visible = false;
+            }
+            else if (hak == 1)
+            {
+                pbHak2.Visible = false;
+            }
+            else if (hak == 0)
+            {
+                pbHak3.Visible = false;
+                timerPacmanDirection.Stop();
+                timerGhostDirection.Stop();
+                DialogResult result = MessageBox.Show("Puanınız=> " + score + "\n Tekrar Oynamak İster Misiniz?", "Pacman", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+                if (result == DialogResult.OK)
+                {
+                    ResetEverything();
+                    
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    ApplicationExit();
+                }
+            }
+
+            pbPacman.Location = new Point(37, 37);
+            pbPacman.BackgroundImage = imageList1.Images[0];
+            pbPacman.BackColor = Color.Transparent;
+            direction = "Right";
+        }
+
+        public void ApplicationExit()
+        {
+            Application.Exit();
+        }
+
+        public void ResetEverything()
+        {
+            hak = 3;
+            pbHak1.Visible = true;
+            pbHak2.Visible = true;
+            pbHak3.Visible = true;
+            for (int i = 0; i <210; i++)
+            {
+                this.Controls.Remove(ForageList[i]);
+               
+            }
+            ForageList.Clear();
+            timerPacmanDirection.Start();
+            CreateForage();
+            timerGhostDirection.Start();
+            ghost1.Location = new Point(390, 240);
+            ghost3.Location = new Point(490, 240);
+            ghost2.Location = new Point(590, 240);
+            ghost1.BringToFront();
+            ghost2.BringToFront();
+            ghost3.BringToFront();
+            score = 0;
+            lblScore.Text = 0.ToString();
+        }
+
+        public void CreateForage()
+        {
+            for (int i = 50; i <= 1050; i += 50)
+            {
+                for (int j = 50; j <= 500; j += 50)
+                {
+                    PictureBox forage = new PictureBox();
+                 
+                    forage.Name = "forage" + i + j;
+                    forage.Size = new Size(20, 20);
+                    forage.Location = new Point(i, j);
+                    forage.BackgroundImageLayout = ImageLayout.Stretch;
+                    forage.BackColor = Color.Transparent;
+                    forage.BackgroundImage = Properties.Resources.Forage;
+                    forage.BackgroundImageLayout = ImageLayout.Stretch;
+                    ForageList.Add(forage);
+                    this.Controls.Add(forage);
+                }
+            }
+            ForageList[0].Visible = false;
+            this.Controls.Remove(ForageList[0]);
+            this.Controls.Remove(ForageList[65]);
+            this.Controls.Remove(ForageList[64]);
+            this.Controls.Remove(ForageList[63]);
+            this.Controls.Remove(ForageList[75]);
+            this.Controls.Remove(ForageList[85]);
+            this.Controls.Remove(ForageList[95]);
+            this.Controls.Remove(ForageList[105]);
+            this.Controls.Remove(ForageList[115]);
+            this.Controls.Remove(ForageList[125]);
+            this.Controls.Remove(ForageList[124]);
+            this.Controls.Remove(ForageList[123]);
+        }
+
+        public void CreateWall()
+        {
+            for (int i = 0; i <= 1100; i += 20)
+            {
+                for (int j = 0; j <= 540; j += 20)
+                {
+                    if (i == 0 || j == 0 || i == 1100 || j == 540)
+                    {
+                        Label wall = new Label();
+                        wall.Name = "Wall" + i + j;
+                        wall.Size = new Size(15, 15);
+                        wall.Location = new Point(i, j);
+                        wall.BackColor = Color.Blue;
+                        wall.BringToFront();
+                        this.Controls.Add(wall);
+                    }
+
+                    if (((i >= 340 && i <= 360) && (j >= 200 && j <= 300)) || ((j >= 300 && j <= 320) && (i >= 340 && i <= 660)) || ((i >= 640 && i <= 660) && (j <= 300 && j >= 200)))
+                    {
+                        Label wall2 = new Label();
+                        wall2.Name = "Wall2" + i + j;
+                        wall2.Size = new Size(15, 15);
+                        wall2.Location = new Point(i, j);
+                        wall2.BackColor = Color.Blue;
+                        wall2.BringToFront();
+                        this.Controls.Add(wall2);
+                    }
+
+                }
+            }
+
+        }
+
+        public void CreateGhost()
+        {
+            ghost1 = new PictureBox();
+            ghost2 = new PictureBox();
+            ghost3 = new PictureBox();
+
+            ghost1.Location = new Point(390, 240);
+            ghost1.BackgroundImage = imageListGhost.Images[0];
+            ghost1.BackgroundImageLayout = ImageLayout.Zoom;
+            ghost1.Size = new Size(45, 45);
+            ghost1.BackColor = Color.Transparent;
+
+            ghost2.Location = new Point(590, 240);
+            ghost2.BackgroundImage = imageListGhost.Images[1];
+            ghost2.BackgroundImageLayout = ImageLayout.Zoom;
+            ghost2.Size = new Size(45, 45);
+            ghost2.BackColor = Color.Transparent;
+
+            ghost3.Location = new Point(490, 240);
+            ghost3.BackgroundImage = imageListGhost.Images[2];
+            ghost3.BackgroundImageLayout = ImageLayout.Zoom;
+            ghost3.Size = new Size(45, 45);
+            ghost3.BackColor = Color.Transparent;
+
+            ghost1.BringToFront();
+            ghost2.BringToFront();
+            ghost3.BringToFront();
+            this.Controls.Add(ghost1);
+            this.Controls.Add(ghost2);
+            this.Controls.Add(ghost3);
+        }
+
+        private void FormPacman_Load(object sender, EventArgs e)
+        {
+            timerPacmanDirection.Start();
+            CreateForage();
+            CreateWall();
+            CreateGhost();
+            timerGhostDirection.Start();
+            ghost1.BringToFront();
+            ghost2.BringToFront();
+            ghost3.BringToFront();
+        }
+
+        private void timerPacmanDirection_Tick(object sender, EventArgs e)
+        {
+            timerPacmanDirection.Interval = 100;
+            Movement();
+            lblScore.Text = score.ToString();
+        }
+
+        private void FormPacman_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Up)
+            {
+                direction = "Up";
+                pbPacman.BackgroundImage = imageList1.Images[3];
+                pbPacman.BackColor = Color.Transparent;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                direction = "Down";
+                pbPacman.BackgroundImage = imageList1.Images[2];
+                pbPacman.BackColor = Color.Transparent;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                direction = "Right";
+                pbPacman.BackgroundImage = imageList1.Images[0];
+                pbPacman.BackColor = Color.Transparent;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                direction = "Left";
+                pbPacman.BackgroundImage = imageList1.Images[1];
+                pbPacman.BackColor = Color.Transparent;
+            }
+        }
+
+        public void Movement()
+        {
+            if(score == 198)
+            {
+                Win();
+            }
+
+            if (direction == "Up")
+            {
+                if (50 <= pbPacman.Top && !(pbPacman.Top == 337 && (pbPacman.Left == 337 || pbPacman.Left == 387 || pbPacman.Left == 437 || pbPacman.Left == 487 || pbPacman.Left == 537 || pbPacman.Left == 587 || pbPacman.Left == 637)))
+                {
+                    pbPacman.Top -= 50;
+                    foreach (var item in this.ForageList)
+                    {
+                        if (pbPacman.Top < item.Bottom && pbPacman.Bottom > item.Top && pbPacman.Left <= item.Right && pbPacman.Right >= item.Left)
+                        {
+                            if (item.Visible == true)
+                                score++;
+                            Controls.Remove(item);
+                            item.Visible = false;
+                        }
+                    }
+
+                    if ((pbPacman.Top < ghost1.Bottom && pbPacman.Bottom > ghost1.Top && pbPacman.Left <= ghost1.Right && pbPacman.Right >= ghost1.Left) || (pbPacman.Top < ghost2.Bottom && pbPacman.Bottom > ghost2.Top && pbPacman.Left <= ghost2.Right && pbPacman.Right >= ghost2.Left) || (pbPacman.Top < ghost3.Bottom && pbPacman.Bottom > ghost3.Top && pbPacman.Left <= ghost3.Right && pbPacman.Right >= ghost3.Left))
+                    {
+                        GoToReturn();
+                    }
+
+                }
+            }
+            else if (direction == "Down")
+            {
+
+                if (this.ClientSize.Height - 30 >= pbPacman.Bottom && !(pbPacman.Top == 137 && (pbPacman.Left == 637 && pbPacman.Right == 687)) && !(pbPacman.Top == 137 && (pbPacman.Left == 337 && pbPacman.Right == 387)) && !(pbPacman.Top == 237 && (pbPacman.Left == 387 || pbPacman.Left == 437 || pbPacman.Left == 487 || pbPacman.Left == 537 || pbPacman.Left == 587)))
+                {
+                    pbPacman.Top += 50;
+                    foreach (var item in this.ForageList)
+                    {
+                        if (pbPacman.Top < item.Bottom && pbPacman.Bottom > item.Top && pbPacman.Left <= item.Right && pbPacman.Right >= item.Left)
+                        {
+
+                            if (item.Visible == true)
+                                score++;
+                            Controls.Remove(item);
+                            item.Visible = false;
+                        }
+                    }
+
+                    if ((pbPacman.Top < ghost1.Bottom && pbPacman.Bottom > ghost1.Top && pbPacman.Left <= ghost1.Right && pbPacman.Right >= ghost1.Left) || (pbPacman.Top < ghost2.Bottom && pbPacman.Bottom > ghost2.Top && pbPacman.Left <= ghost2.Right && pbPacman.Right >= ghost2.Left) || (pbPacman.Top < ghost3.Bottom && pbPacman.Bottom > ghost3.Top && pbPacman.Left <= ghost3.Right && pbPacman.Right >= ghost3.Left))
+                    {
+                        GoToReturn();
+                    }
+                }
+
+            }
+            else if (direction == "Right")
+            {
+                if (1050 >= pbPacman.Right && !(pbPacman.Right == 337 && (pbPacman.Top == 187 || pbPacman.Top == 237 || pbPacman.Top == 287)) && !(pbPacman.Right == 637 && (pbPacman.Top == 187 || pbPacman.Top == 237)))
+                {
+                    pbPacman.Left += 50;
+
+
+                    foreach (var item in this.ForageList)
+                    {
+                        if (pbPacman.Top <= item.Bottom && pbPacman.Bottom >= item.Top && pbPacman.Left < item.Right && pbPacman.Right > item.Left)
+                        {
+                            if (item.Visible == true)
+                                score++;
+                            Controls.Remove(item);
+                            item.Visible = false;
+                        }
+                    }
+
+                    if ((pbPacman.Top <= ghost1.Bottom && pbPacman.Bottom >= ghost1.Top && pbPacman.Left < ghost1.Right && pbPacman.Right > ghost1.Left) || (pbPacman.Top <= ghost2.Bottom && pbPacman.Bottom >= ghost2.Top && pbPacman.Left < ghost2.Right && pbPacman.Right > ghost2.Left) || (pbPacman.Top <= ghost3.Bottom && pbPacman.Bottom >= ghost3.Top && pbPacman.Left < ghost3.Right && pbPacman.Right > ghost3.Left))
+                    {
+                        GoToReturn();
+                    }
+
+                }
+
+            }
+            else if (direction == "Left")
+            {
+                if (50 <= pbPacman.Left && !(pbPacman.Right == 737 && (pbPacman.Top == 187 || pbPacman.Top == 237 || pbPacman.Top == 287)) && !(pbPacman.Right == 437 && (pbPacman.Top == 187 || pbPacman.Top == 237)))
+                {
+                    pbPacman.Left -= 50;
+                    foreach (var item in this.ForageList)
+                    {
+                        if (pbPacman.Top <= item.Bottom && pbPacman.Bottom >= item.Top && pbPacman.Left < item.Right && pbPacman.Right > item.Left)
+                        {
+                            if (item.Visible == true)
+                                score++;
+                            Controls.Remove(item);
+                            item.Visible = false;
+                        }
+                    }
+
+                    if ((pbPacman.Top <= ghost1.Bottom && pbPacman.Bottom >= ghost1.Top && pbPacman.Left < ghost1.Right && pbPacman.Right > ghost1.Left) || (pbPacman.Top <= ghost2.Bottom && pbPacman.Bottom >= ghost2.Top && pbPacman.Left < ghost2.Right && pbPacman.Right > ghost2.Left) || (pbPacman.Top <= ghost3.Bottom && pbPacman.Bottom >= ghost3.Top && pbPacman.Left < ghost3.Right && pbPacman.Right > ghost3.Left))
+                    {
+                        GoToReturn();
+                    }
+
+
+                }
+
+            }
+        }
+
+        private void timerGhostDirection_Tick(object sender, EventArgs e)
+        {
+            timerGhostDirection.Interval = 200;
+            string ghost1Move = directionsArray[rnd.Next(0, 4)];
+            string ghost2Move = directionsArray[rnd.Next(0, 4)];
+            string ghost3Move = directionsArray[rnd.Next(0, 4)];
+            GhostMove(ghost1Move, ghost1);
+            GhostMove(ghost2Move, ghost2);
+            GhostMove(ghost3Move, ghost3);
+        }
+
+        public void GhostMove(string directionGhost, PictureBox ghost)
+        {
+
+            if (directionGhost == "Up")
+            {
+                if (50 <= ghost.Top && !(ghost.Top == 340 && (ghost.Left == 340 || ghost.Left == 390 || ghost.Left == 440 || ghost.Left == 490 || ghost.Left == 540 || ghost.Left == 590 || ghost.Left == 640)))
+                {
+                    ghost.Top -= 50;
+
+                    if (pbPacman.Top < ghost.Bottom && pbPacman.Bottom > ghost.Top && pbPacman.Left <= ghost.Right && pbPacman.Right >= ghost.Left)
+                    {
+                        GoToReturn();
+                    }
+                }
+            }
+            else if (directionGhost == "Down")
+            {
+
+                if (this.ClientSize.Height - 30 >= ghost.Bottom && !(ghost.Top == 140 && (ghost.Left == 640 && ghost.Right == 685)) && !(ghost.Top == 140 && (ghost.Left == 340 && ghost.Right == 385)) && !(ghost.Top == 240 && (ghost.Left == 390 || ghost.Left == 440 || ghost.Left == 490 || ghost.Left == 540 || ghost.Left == 590)))
+                {
+                    ghost.Top += 50;
+
+                    if (pbPacman.Top < ghost.Bottom && pbPacman.Bottom > ghost.Top && pbPacman.Left <= ghost.Right && pbPacman.Right >= ghost.Left)
+                    {
+                        GoToReturn();
+                    }
+                }
+
+            }
+            else if (directionGhost == "Right")
+            {
+                if (1050 >= ghost.Right && !(ghost.Right == 335 && (ghost.Top == 190 || ghost.Top == 240 || ghost.Top == 290)) && !(ghost.Right == 635 && (ghost.Top == 190 || ghost.Top == 240)))
+                {
+                    ghost.Left += 50;
+
+                    if (pbPacman.Top <= ghost.Bottom && pbPacman.Bottom >= ghost.Top && pbPacman.Left < ghost.Right && pbPacman.Right > ghost.Left)
+                    {
+                        GoToReturn();
+                    }
+                }
+
+            }
+            else if (directionGhost == "Left")
+            {
+                if (50 <= ghost.Left && !(ghost.Right == 735 && (ghost.Top == 190 || ghost.Top == 240 || ghost.Top == 290)) && !(ghost.Right == 435 && (ghost.Top == 190 || ghost.Top == 240)))
+                {
+                    ghost.Left -= 50;
+
+                    if (pbPacman.Top <= ghost.Bottom && pbPacman.Bottom >= ghost.Top && pbPacman.Left < ghost.Right && pbPacman.Right > ghost.Left)
+                    {
+                        GoToReturn();
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
